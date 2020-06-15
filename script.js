12ac34ef3afd8679b737f18931c089d0a33487a4
Laurel Wright <wright.laureld@gmail.com>
2020-06-15 17:03:11 +0200

    Back to top button working

--- script.js
+++ script.js
@@ -49,4 +49,21 @@ $(document).ready(function () {
 });
 
 // Back to Top Button
+myButton = document.getElementById("myBtn");
 
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block"
+  } else {
+    myButton.style.display = "none"
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
